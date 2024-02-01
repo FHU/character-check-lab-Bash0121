97cbbd7c5913e4d1e7edeafcc3941fc1716f1f45
Bash0121 <Robert.smith@students.fhu.edu>
2024-02-01 17:56:49 -0600

    Update main.py
    first draft, not returning anything.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
-
+   for word in [index]:
+      if word==int:
+         print("digit")
+      elif word==str:
+         print("Letter")
+      elif word==" ":
+         print("White Space")
+      '''else:
+         print("unknown")'''
+   return
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
