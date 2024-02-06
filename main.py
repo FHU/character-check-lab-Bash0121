d23c202e3fe8e216f0e75b4b3f0109c9ea8ee3ea
Bash0121 <Robert.smith@students.fhu.edu>
2024-02-06 15:35:18 -0600

    Update main.py
    update 1

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   for word in [index]:
-      if word==int:
-         print("digit")
-      elif word==str:
-         print("Letter")
-      elif word==" ":
-         print("White Space")
-      else:
-         print("unknown")
-   return
+   word_chracter=word[index]
+   
+   whitespace=" "
+   string=word_chracter.isalpha()
+   numbers=('1', '2', '4', '5', '6', '7', '8', '9')
+
+
+   if word_chracter in numbers:  
+         return "digit"
+   elif string==True:
+         return "letter"
+   elif word_chracter in whitespace:
+         return "white Space"
+   else:
+         return "unknown"
+   
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
